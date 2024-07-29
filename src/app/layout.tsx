d793b5e047b1a7a126f9e873899c88833b3062eb
Sanjit Thangarasu <isanjit3@gmail.com>
2024-07-29 02:02:08 -0400

    Added vercel analytics and speed-insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,11 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+// Analytics and Speed Insights
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +21,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body 
+        className={inter.className}>{children}
+        <Analytics />
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
