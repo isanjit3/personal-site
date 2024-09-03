93e1001700504928a756bb2ed21f3aa181bfab07
Sanjit Thangarasu <isanjit3@gmail.com>
2024-09-03 11:18:20 -0400

    Refactor site with shadcn/ui and added mouse gradient effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,115 +1,15 @@
-"use client";
-
-import { ReactTyped } from "react-typed";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFile } from "@fortawesome/free-solid-svg-icons";
-import { faLinkedin, faGithub, faSpotify } from "@fortawesome/free-brands-svg-icons";
-import { useState } from "react";
-import { Expand } from "@theme-toggles/react";
-import "@theme-toggles/react/css/Expand.css";
+import { Hero } from "@/components/sections/hero";
+import { NavigationBar } from "@/components/sections/navbar";
+import GradientReveal from "@/components/sections/gradient";
 
 export default function Home() {
-  const [isLightMode, setIsLightMode] = useState(false);
-
-  const handleToggle = () => {
-    setIsLightMode(!isLightMode);
-    document.body.classList.toggle("light-mode", !isLightMode);
-    document.body.classList.toggle("dark-mode", isLightMode);
-  };
-
   return (
-    <div className="fade-in transition-all duration-500 ease-in-out">
-      <main
-        className={`relative flex flex-col items-center justify-center h-screen p-8 transition-all duration-500 ease-in-out ${
-          isLightMode ? "bg-lightBackground text-customGray" : "bg-darkBackground text-white"
-        }`}
-      >
-        <div className="absolute top-16 right-16 scale-2x">
-          {/* @ts-ignore */}
-          <Expand
-            toggled={isLightMode}
-            onToggle={handleToggle}
-            id="theme-toggle"
-            name="theme-toggle"
-            type="button"
-            aria-label="Toggle Theme"
-          />
-        </div>
-        <div className="text-center">
-          <h1 className="text-6xl font-bold mb-6">
-            Hi, my name is <span className="text-customRed">Sanjit</span> <span role="img" aria-label="wave">👋</span>
-          </h1>
-          <p className="text-3xl mb-10">
-            I am a{""}
-            <ReactTyped
-              strings={["&nbsp;software engineer", "&nbsp;change maker", "n innovator"]}
-              typeSpeed={80}
-              backSpeed={50}
-              backDelay={1300}
-              loop
-              showCursor={true}
-              cursorChar="|"
-            />
-          </p>
-          <div className="flex flex-wrap justify-center gap-4 sm:gap-6 w-full unselectable">
-            {/* Resume Button */}
-            <a
-              href="https://eblws9kpxcehafgs.public.blob.vercel-storage.com/files/Thangarasu_Sanjit_Resume.pdf"
-              target="_blank"
-              className={`w-full sm:w-auto bg-red-500 text-white font-bold py-3 px-6 rounded transition duration-300 ease-in-out group ${
-                isLightMode
-                  ? "hover:bg-red-200 hover:text-red-500"
-                  : "hover:bg-white hover:text-red-500"
-              } flex items-center justify-center space-x-2 text-base sm:text-lg`}
-            >
-              <FontAwesomeIcon icon={faFile} />
-              <span>Resume</span>
-            </a>
-
-            {/* LinkedIn Button */}
-            <a
-              href="https://www.linkedin.com/in/tsanjit/"
-              target="_blank"
-              className={`w-full sm:w-auto bg-blue-600 text-white font-bold py-3 px-6 rounded transition duration-300 ease-in-out group ${
-                isLightMode
-                  ? "hover:bg-blue-200 hover:text-blue-600"
-                  : "hover:bg-white hover:text-blue-600"
-              } flex items-center justify-center space-x-2 text-base sm:text-lg`}
-            >
-              <FontAwesomeIcon icon={faLinkedin} />
-              <span>LinkedIn</span>
-            </a>
-
-            {/* GitHub Button */}
-            <a
-              href="https://github.com/isanjit3"
-              target="_blank"
-              className={`w-full sm:w-auto bg-purple-600 text-white font-bold py-3 px-6 rounded transition duration-300 ease-in-out group ${
-                isLightMode
-                  ? "hover:bg-purple-200 hover:text-purple-600"
-                  : "hover:bg-white hover:text-purple-600"
-              } flex items-center justify-center space-x-2 text-base sm:text-lg`}
-            >
-              <FontAwesomeIcon icon={faGithub} />
-              <span>GitHub</span>
-            </a>
-
-            {/* Spotify Button */}
-            <a
-              href="https://open.spotify.com/user/qfat5ryfmyfo9jz7emk4z6c76?si=b345e46a8d1943f4"
-              target="_blank"
-              className={`w-full sm:w-auto bg-green-500 text-white font-bold py-3 px-6 rounded transition duration-300 ease-in-out group ${
-                isLightMode
-                  ? "hover:bg-green-200 hover:text-green-500"
-                  : "hover:bg-white hover:text-green-500"
-              } flex items-center justify-center space-x-2 text-base sm:text-lg`}
-            >
-              <FontAwesomeIcon icon={faSpotify} />
-              <span>Spotify</span>
-            </a>
-          </div>
+    
+      <main className="flex flex-col w-full min-h-screen p-12 relative">
+        <NavigationBar />
+        <div className="flex flex-1 items-start justify-center mt-60">
+          <Hero />
         </div>
       </main>
-    </div>
   );
 }
---
+++ src/components/sections/hero.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { ReactTyped } from "react-typed";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFile } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedin, faGithub, faSpotify } from "@fortawesome/free-brands-svg-icons";
+import { Button } from "@/components/ui/button";
+
+export function Hero() {
+    return (
+        <div className="flex flex-col items-center text-center gap-y-10">
+            <h1 className="font-bold tracking-wide text-6xl">
+                Hi, my name is Sanjit 👋
+            </h1>
+            <p className="text-3xl">
+                I am a
+                <ReactTyped
+                    strings={["&nbsp;software engineer", "&nbsp;change maker", "n innovator"]}
+                    typeSpeed={80}
+                    backSpeed={50}
+                    backDelay={1300}
+                    loop
+                    showCursor={true}
+                    cursorChar="|"
+                />
+            </p>
+            <div className="shrink-hover unselectable grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
+                <a href="https://eblws9kpxcehafgs.public.blob.vercel-storage.com/files/Thangarasu_Sanjit_Resume.pdf" target="_blank" rel="noopener noreferrer">
+                    <Button 
+                        className="flex flex-row items-center gap-x-2.5 bg-red-500 text-white hover:bg-red-100 hover:text-red-500">
+                        <FontAwesomeIcon icon={faFile} />
+                        Resume
+                    </Button>
+                </a>
+                <a href="https://www.linkedin.com/in/tsanjit/" target="_blank" rel="noopener noreferrer">
+                    <Button
+                        className="flex flex-row items-center gap-x-2.5 bg-blue-500 text-white hover:bg-blue-100 hover:text-blue-500">             
+                        <FontAwesomeIcon icon={faLinkedin} />
+                        LinkedIn
+                    </Button>
+                </a>
+                <a href="https://github.com/isanjit3" target="_blank" rel="noopener noreferrer">
+                    <Button
+                        className="flex flex-row items-center gap-x-2.5 bg-purple-500 text-white hover:bg-purple-100 hover:text-purple-500">
+                        <FontAwesomeIcon icon={faGithub} />
+                        GitHub
+                    </Button>
+                </a>
+                <a href="https://open.spotify.com/user/qfat5ryfmyfo9jz7emk4z6c76?si=894cca65877242eb" target="_blank" rel="noopener noreferrer">
+                    <Button
+                        className="flex flex-row items-center gap-x-2.5 bg-green-500 text-white hover:bg-green-100 hover:text-green-500">
+                        <FontAwesomeIcon icon={faSpotify} />
+                        Spotify
+                    </Button>
+                </a>
+            </div>
+        </div>
+    );
+}
