3c4c3170aac8053bdf0c9e01214f6f88620135e3
Sanjit Thangarasu <isanjit3@gmail.com>
2024-09-04 23:03:06 -0400

    Better css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,13 @@ import Badge from "@/components/sections/badge"; // Assuming Badge is in your co
 
 export default function Home() {
   return (
-    <main className="flex flex-col w-full min-h-screen p-12 relative">
+    <main className="flex flex-col w-full min-h-screen p-12 relative overflow-hidden">
       <NavigationBar />
       <div className="flex flex-1 items-start justify-center mt-40">
         <Hero />
       </div>
       {/* Overlay the Badge */}
-      <div className="absolute top-0 left-[-8rem] p-4" style={{ width: '100vw', height: '100vh' }}>
+      <div className="absolute top-0 left-[-15rem] p-4" style={{ width: '120vw', height: '100vh' }}>
         <Badge />
       </div>
     </main>
