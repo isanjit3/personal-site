ae610ee2335634f9834df9d264e32c7c650077f1
Sanjit Thangarasu <isanjit3@gmail.com>
2025-02-05 14:56:09 -0500

    new homepage setup

--- src/components/sections/about.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export function About() {
-    return (
-        <div id="about" className="flex flex-col min-h-screen items-center text-center gap-10">
-            <h1>ABOUT ME</h1>
-        </div>
-    );
-}
