02b51c9d85b2c06f91c0ccc708447d4418872a34
Sanjit Thangarasu <isanjit3@gmail.com>
2025-02-05 23:43:37 -0500

    New homepage done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import { useState, useEffect } from "react";
 import { SplineScene } from "@/components/robot/splite";
 import { BoxReveal } from "@/components/ui/box-reveal";
 import { Typewriter } from "@/components/ui/typewriter";
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Home() {
@@ -39,7 +38,7 @@ export default function Home() {
                     <Typewriter
                       text={[
                         "a software engineer",
-                        "an entrepreneur",
+                        "a problem solver",
                         "an innovator",
                         "a change maker",
                       ]}
@@ -54,33 +53,48 @@ export default function Home() {
               </BoxReveal>
             </div>
           </div>
-          <div className="flex-1 flex pl-[9.15rem]">
+          <div className="flex-1 flex pl-[8rem]">
             {/* New vertically stacked links with box reveal */}
-            <div className="space-y-8 text-[1.5rem]">
+            <div
+              className={`max-w-lg pl-15 overflow-hidden transition-opacity duration-500 ${
+                showBox ? "opacity-100" : "opacity-0"
+              }`}
+            >
+            <div className="space-y-9 text-[2rem]">
               <BoxReveal boxColor="#5046e6" duration={0.5}>
-                <Link href="/about" className="block">
+                <div className="relative group">
+                <Link href="https://eblws9kpxcehafgs.public.blob.vercel-storage.com/files/Thangarasu_Sanjit_Resume.pdf" target="_blank" className="relative inline-block px-4 py-2 transition-all duration-300 ease-in-out group-hover:text-white">
                   <span className="relative z-10">
-                    Hover Me
+                    Resume
                     <span className="absolute -bottom-1 left-0 w-full h-0.5 bg-[#5046e6] transform origin-left transition-transform duration-300 ease-in-out group-hover:scale-x-0"></span>
                   </span>
+                  <span className="absolute inset-0 bg-red-500 transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out origin-left"></span>
                 </Link>
+                </div>
               </BoxReveal>
               <BoxReveal boxColor="#5046e6" duration={0.5}>
-                <Link href="/projects" className="block">
+                <div className="relative group">
+                <Link href="https://github.com/isanjit3" target="_blank" className="relative inline-block px-4 py-2 transition-all duration-300 ease-in-out group-hover:text-white">
                   <span className="relative z-10">
-                    Hover Me
+                    GitHub
                     <span className="absolute -bottom-1 left-0 w-full h-0.5 bg-[#5046e6] transform origin-left transition-transform duration-300 ease-in-out group-hover:scale-x-0"></span>
                   </span>
+                  <span className="absolute inset-0 bg-purple-500 transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out origin-left"></span>
                 </Link>
+                </div>
               </BoxReveal>
               <BoxReveal boxColor="#5046e6" duration={0.5}>
-                <Link href="/contact" className="block">
+                <div className="relative group">
+                <Link href="https://www.linkedin.com/in/tsanjit/" target="_blank" className="relative inline-block px-4 py-2 transition-all duration-300 ease-in-out group-hover:text-white">
                   <span className="relative z-10">
-                    Hover Me
+                    LinkedIn
                     <span className="absolute -bottom-1 left-0 w-full h-0.5 bg-[#5046e6] transform origin-left transition-transform duration-300 ease-in-out group-hover:scale-x-0"></span>
                   </span>
+                  <span className="absolute inset-0 bg-blue-500 transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 ease-in-out origin-left"></span>
                 </Link>
+                </div>
               </BoxReveal>
+            </div>
             </div>
           </div>
         </div>
